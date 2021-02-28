076f1ba279dd417c33918d1d2e6d9e016bba251b
fengjijiao <16521693+fengjijiao@users.noreply.github.com>
2021-02-28 20:05:37 +0800

    fix send message error bug

--- pkg/httphandler/httpServer.go
+++ pkg/httphandler/httpServer.go
@@ -8,8 +8,6 @@ import (
 
 func Run() error {
 	http.HandleFunc(path.Join(conf.Config.BaseUrlPath, "/"), defaultHttpHandler)
-	http.HandleFunc(path.Join(conf.Config.BaseUrlPath, conf.Config.SecurityPrefix, "send"), sendMarkDownHttpHandler)
-	http.HandleFunc(path.Join(conf.Config.BaseUrlPath, conf.Config.SecurityPrefix, "markdown", "send"), sendMarkDownHttpHandler)
-	http.HandleFunc(path.Join(conf.Config.BaseUrlPath, conf.Config.SecurityPrefix, "text", "send"), sendTextHttpHandler)
+	http.HandleFunc(path.Join(conf.Config.BaseUrlPath, conf.Config.SecurityPrefix, "send"), sendMessageHttpHandler)
     return http.ListenAndServe(conf.Config.HttpServerListen, nil)
 }
--- pkg/httphandler/sendHttpHandler.go
+++ pkg/httphandler/sendHttpHandler.go
@@ -6,7 +6,6 @@ import (
 	"github.com/fengjijiao/dingding-push-restful-api/pkg/conf"
 	"github.com/fengjijiao/dingding-push-restful-api/pkg/logio"
 	"go.uber.org/zap"
-	//"path"
 	"github.com/fengjijiao/dingding-push-restful-api/pkg/sqlhandler"
 	"encoding/json"
 	"github.com/imroc/req"
@@ -15,18 +14,18 @@ import (
 type SendInfo struct {
 	Msg struct {
 		Markdown struct {
-			Text  string `json:"text"`
-			Title string `json:"title"`
-		} `json:"markdown"`
+			Text  string `json:"text,omitempty"`
+			Title string `json:"title,omitempty"`
+		} `json:"markdown,omitempty"`
 		Text struct {
-			Content string `json:"content"`
-		} `json:"text"`
-		MsgType string `json:"msgtype"`
-	} `json:"msg"` //不超过2048字节
-	ToAllUser  string `json:"to_all_user"`
+			Content string `json:"content,omitempty"`
+		} `json:"text,omitempty"`
+		MsgType string `json:"msgtype,omitempty"`
+	} `json:"msg,omitempty"` //不超过2048字节
+	ToAllUser  bool `json:"to_all_user,omitempty"`
 	AgentId    int `json:"agent_id"`
-	DeptIdList string `json:"dept_id_list"`
-	UserIdList string `json:"userid_list"`
+	DeptIdList string `json:"dept_id_list,omitempty"`
+	UserIdList string `json:"userid_list,omitempty"`
 }
 
 type ErrorInfo struct {
@@ -34,81 +33,61 @@ type ErrorInfo struct {
 	ErrMsg string `json:"errmsg"`
 }
 
-func sendMarkDownHttpHandler(w http.ResponseWriter, hr *http.Request) {
+func sendMessageHttpHandler(w http.ResponseWriter, hr *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	msgtype := hr.URL.Query().Get("msgtype")
+	markdownFlag := true
+	if len(msgtype) <= 0 {
+		msgtype = "text"
+		markdownFlag = false
+	}else {
+		markdownFlag = (msgtype == "markdown")
+	}
+	var title string
+	var body string
+	var context string
 	hr.ParseForm()
-	title := hr.FormValue("title")
-	body := hr.FormValue("body")
+	if markdownFlag {
+		title = hr.FormValue("title")
+		body = hr.FormValue("body")
+		if len(title) <= 0 || len(body) <= 0 {
+			json.NewEncoder(w).Encode(&ErrorInfo{-1, "send message failed, missing required parameters!"})
+			return
+		}
+	}else {
+		context = hr.FormValue("context")
+		if len(context) <= 0 {
+			json.NewEncoder(w).Encode(&ErrorInfo{-1, "send message failed, missing required parameters!"})
+			return
+		}
+	}
+	toDeptFlag := false
 	touser := hr.FormValue("touser")
 	toparty := hr.FormValue("toparty")
-	if len(title) <= 0 || len(body) <= 0 {
+	if len(touser) <= 0 {
+		toDeptFlag = true
+	}
+	if len(touser) <= 0 && len(toparty) <= 0 {
 		json.NewEncoder(w).Encode(&ErrorInfo{-1, "send message failed, missing required parameters!"})
 		return
 	}
 	var res ErrorInfo
 	var sendInfo SendInfo
-	if len(touser) > 0 {
-		sendInfo.UserIdList = touser
-	}else if len(toparty) > 0 {
+	sendInfo.AgentId = conf.Config.DingdingAgentId
+	if toDeptFlag {
 		sendInfo.DeptIdList = toparty
 	}else {
-		json.NewEncoder(w).Encode(&ErrorInfo{-1, "send message failed, missing required parameters!"})
-		return
-	}
-	sendInfo.Msg.MsgType = "markdown"
-	sendInfo.Msg.Markdown.Title = title
-	sendInfo.Msg.Markdown.Text = body
-	sendInfo.AgentId = conf.Config.DingdingAgentId
-	param, err := json.Marshal(&sendInfo)
-	if err != nil {
-		json.NewEncoder(w).Encode(&ErrorInfo{-1, err.Error()})
-		logio.Logger.Error("sendHttpHandler: ", zap.Error(err))
-		return
-	}
-	token, err := sqlhandler.GetToken()
-	if err != nil {
-		json.NewEncoder(w).Encode(&ErrorInfo{-1, err.Error()})
-		logio.Logger.Error("sendHttpHandler: ", zap.Error(err))
-		return
-	}
-	r, err := req.Post(fmt.Sprintf(`https://oapi.dingtalk.com/topapi/message/corpconversation/asyncsend_v2?access_token=%s`, token), param)
-	if err != nil {
-		json.NewEncoder(w).Encode(&ErrorInfo{-1, err.Error()})
-		logio.Logger.Error("sendHttpHandler: ", zap.Error(err))
-		return
-	}
-	r.ToJSON(&res)
-	if res.ErrCode == 0 {
-		json.NewEncoder(w).Encode(&ErrorInfo{0, "send message success!"})
-	}else {
-		json.NewEncoder(w).Encode(&ErrorInfo{-1, "send message failed!"+res.ErrMsg})
-	}
-}
-
-func sendTextHttpHandler(w http.ResponseWriter, hr *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	hr.ParseForm()
-	context := hr.FormValue("context")
-	touser := hr.FormValue("touser")
-	toparty := hr.FormValue("toparty")
-	if len(context) <= 0 {
-		json.NewEncoder(w).Encode(&ErrorInfo{-1, "send message failed, missing required parameters!"})
-		return
-	}
-	var res ErrorInfo
-	var sendInfo SendInfo
-	if len(touser) > 0 {
 		sendInfo.UserIdList = touser
-	}else if len(toparty) > 0 {
-		sendInfo.DeptIdList = toparty
-	}else {
-		json.NewEncoder(w).Encode(&ErrorInfo{-1, "send message failed, missing required parameters!"})
-		return
 	}
-	sendInfo.Msg.MsgType = "text"
-	sendInfo.AgentId = conf.Config.DingdingAgentId
-	sendInfo.Msg.Text.Content = context
+	sendInfo.Msg.MsgType = msgtype
+	if markdownFlag {
+		sendInfo.Msg.Markdown.Text = body
+		sendInfo.Msg.Markdown.Title = title
+	}else {
+		sendInfo.Msg.Text.Content = context
+	}
 	param, err := json.Marshal(&sendInfo)
+	fmt.Printf("param: %s\n", string(param))
 	if err != nil {
 		json.NewEncoder(w).Encode(&ErrorInfo{-1, err.Error()})
 		logio.Logger.Error("sendHttpHandler: ", zap.Error(err))
